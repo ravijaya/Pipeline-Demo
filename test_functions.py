1c567a4f7810e8221bf81273ced09389b62aa486
ravijaya <ravi.goglobium@gmail.com>
2020-07-15 21:46:22 +0530

    replaced tabs with space for the indentation

--- test_functions.py
+++ test_functions.py
@@ -10,8 +10,8 @@ class TestFunctions(unittest.TestCase):
         self.value1 = 10
         self.value2 = 20
 
-	def test_power(self):
-		self.assertEqual(100000000000000000000, power(self.value1, self.value2))
+    def test_power(self):
+        self.assertEqual(100000000000000000000, power(self.value1, self.value2))
 
     def test_add_values(self):
         self.assertEqual(30, add_two_values(self.value1, self.value2))
